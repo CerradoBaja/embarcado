9e7dc2df4559de0ab08ce9d6e7305f32bf8bc727
Thalles Bogar <thallesbogar@gmail.com>
2016-05-21 14:26:54 -0300

    Vitória sobre o aux!

--- Template/Templade.ino
+++ Template/Templade.ino
@@ -9,12 +9,13 @@
 
 
 // Definição das constantes - INICIO
-#define PIN_INTERRUPCAO_VELOCIDADE 0
-#define PIN_INTERRUPCAO_RPM 4
+#define PIN_INTERRUPCAO_VELOCIDADE 1
+#define PIN_INTERRUPCAO_RPM 0
 #define PIN_TENSAO A3
+#define PIN_TEMP_MOTOR A0
 #define PIN_TEMP_CVT A1
-#define PIN_VELOCIDADE 2
-#define PIN_RPM 19
+#define PIN_VELOCIDADE 3
+#define PIN_RPM 2
 
 #define TIME_THREAD_CALCULO_VELOCIDADE 500
 #define TIME_THREAD_CALCULO_TEMP_TENSAO 5000
@@ -22,13 +23,15 @@
 #define TIME_THREAD_DISPLAY 500
 
 #define ACTIVE_THREAD_CALCULO_VELOCIDADE true
-#define ACTIVE_THREAD_CALCULO_TEMP_TENSAO false
-#define ACTIVE_THREAD_CALCULO_RPM false
-#define ACTIVE_THREAD_DISPLAY false
+#define ACTIVE_THREAD_CALCULO_TEMP_TENSAO true
+#define ACTIVE_THREAD_CALCULO_RPM true
+#define ACTIVE_THREAD_DISPLAY true
 
 //Termos que definem quantos elementos serao utilizados para calculo da Media Movel:
-#define N1 5 //Velocidade.
-#define N2 5 //RPM.
+#define N1 5 //RPM.
+#define N2 5 //Velocidade.
+#define N3 5 //Temperatura CVT.
+#define N4 5 //Temperatura Motor.
 
 #define TRANSMISSAO 4
 #define PERIMETRO_RODA 1.65
@@ -48,15 +51,18 @@ Thread ThreadDisplay;
 ThreadControl cpuDisplay;
 ThreadControl cpuSensores;
 Eeprom distancia;
-MediaMovel RPM(N2);
-MediaMovel velocidade(N1);
+MediaMovel RPM(N1);
+MediaMovel velocidade(N2);
+MediaMovel temperatura_CVT(N3);
+MediaMovel temperatura_motor(N4);
 
 volatile int contadorRPM = 0;
 volatile int contadorVelocidade = 0;
 int aux = 0;
 int tempoAnt = 0;
-unsigned long lastMillis = 0;
 int contador_filtrado = 50;
+unsigned long lastMillisRPM = 0;
+unsigned long lastMillisVelocidade = 0;
 
 // Definição das variáveis globais - FIM
 
@@ -64,7 +70,6 @@ int contador_filtrado = 50;
 void setup(){
 	Serial.begin(9600);
 	//Configuracao dos pinos:
-	pinMode(PIN_TEMP_CVT, INPUT);
 	pinMode(PIN_TENSAO, INPUT);
 	pinMode(PIN_VELOCIDADE, INPUT_PULLUP);
 	pinMode(PIN_RPM, INPUT_PULLUP);
@@ -109,7 +114,18 @@ void loop(){
 
 // Rotina para imprimir informacoes no display - INICIO
 void plotar(){
-	draw(contador_filtrado, 23, 12, 300, 40, 50);
+	//draw(contador_filtrado, 23, 12, 300, 40, 50);
+	Serial.print("Velocidade: ");
+	Serial.print(velocidade.calcularMedia());
+	Serial.print(" | ");
+	Serial.print("Distancia: ");
+	Serial.print(distancia.read());
+	Serial.print(" | ");
+	Serial.print("RPM: ");
+	Serial.print(RPM.calcularMedia());
+	Serial.print(" | ");
+	Serial.print("Temperatura do motor: ");
+	Serial.println(temperatura_motor.calcularMedia());
 }
 // Rotina para imprimir informacoes no display - FIM
 
@@ -117,99 +133,52 @@ void plotar(){
 //Funcao responsavel por ativar as interrupcoes - INICIO:
 void ligarInterrupcoes(){
 	attachInterrupt(PIN_INTERRUPCAO_VELOCIDADE, interrupcao_velocidade,  FALLING);
-	attachInterrupt(PIN_INTERRUPCAO_RPM, interrupcao_rpm_motor, FALLING);
+	attachInterrupt(PIN_INTERRUPCAO_RPM, interrupcao_rpm_motor, RISING);
 }
 //Funcao responsavel por ativar as interrupcoes - FIM.
 
-//Funcao responsavel por desativar as interrupcoes - INICIO:
-void desligarInterrupcoes(){
-	/*detachInterrupt(PIN_INTERRUPCAO_VELOCIDADE);
-	detachInterrupt(PIN_INTERRUPCAO_RPM);*/
-}
-//Funcao responsavel por desativar as interrupcoes - FIM.
-
 //Funcao responsavel pelo calculo da velocidade - INICIO:
 void calculoVelocidade(){
-	desligarInterrupcoes(); //Desativanto interrupcoes para que o calculo da velocidade nao seja interrompido.
 
 	//Declaracao das variaveis usadas localmente:
 	float distanciaPercorrida = contadorVelocidade * DISTANCIA_POR_INTERRUPCAO;
 
 	//Processo para calculo da velocidade:
-	velocidade.setarElementos((distanciaPercorrida/((millis() - lastMillis)/1000.0))*3.6);
-	Serial.print(contadorVelocidade);
-	Serial.print(" | ");
-	Serial.print((millis() - lastMillis)/1000.0);
-	Serial.print(" | ");
-	Serial.print(velocidade.calcularMedia());
-	Serial.print(" | ");
-	Serial.print((distanciaPercorrida/((millis() - lastMillis)/1000.0))*3.6);	
-	Serial.print(" | ");
+	velocidade.setarElementos((distanciaPercorrida/((millis() - lastMillisVelocidade)/1000.0))*3.6);
 	distancia.write(distancia.read() + distanciaPercorrida);
-	Serial.println(distancia.read());
 	contadorVelocidade = 0;
-	lastMillis = millis();
-
-	ligarInterrupcoes(); //Reativando as interrupcoes.
+	lastMillisVelocidade = millis();
 }
 //Funcao responsavel pelo calculo da velocidade - FIM.
 
 //Funcao responsavel pelo calculo da temperatura e da tensao da bateria - INICIO:
-void calculoTempTensao()
-{
-	int tensao = 0;
-	int temperatura1 = 0;
+void calculoTempTensao(){
+	float tensao = 0;
 
 	//Calculo da tensao e da temperatura:
 	tensao = (analogRead(A3)*0.00488*3);   // lendo o divisor de tensao (analogico 3 do arduino) e calculando a tensao.
-	temperatura1 = ((analogRead(A1)*0.488)/2);  // lendo lm35 (analogico 1 do arduino) e calculando a tensao.
+	temperatura_motor.setarElementos((float(analogRead(PIN_TEMP_MOTOR))*5/(1023))/0.01);
+	temperatura_CVT.setarElementos((float(analogRead(PIN_TEMP_CVT))*5/(1023))/0.01);
 }
 //Funcao responsavel pelo calculo da temperatura e da tensao da bateria - FIM.
 
-//Funcao responsavel pelo calculo do RPM - INICIO:
-/*void calculoRPM(){
-	desligarInterrupcoes(); //Desativanto interrupcoes para que o calculo do RPM nao seja interrompido.
-
-	RPM.setarElementos(contador_rpm*60);
-	contador_rpm = 0; //Zerando o contador da interrupcao.
-
-	ligarInterrupcoes(); //Reativando as interrupcoes.
-}*/
-
-void RPMaux(){
-	if(millis() - tempoAnt > 105)
-		aux = 1;
-	else
-		aux = 0;
-	tempoAnt = millis();
-}
-
 void calculoRPM(){
-	desligarInterrupcoes(); //Desativanto interrupcoes para que o calculo do RPM nao seja interrompido.
-	//RPMaux();
-
-	switch(aux){
-		case 1:
-			break;
-		case 0:
-			RPM.setarElementos(contadorRPM);
-			contador_filtrado = RPM.calcularMedia();
-	}
+	// 60000.0 -> conversao de milisegundos para minuto.
+	// millis - lastMillisRPM -> variacao de tempo em milisegundos.
+	// 2* -> quantidade de voltas do girabrequim por centelha da vela.
+	// contadorRPM -> quantidade de interrupcoes obtidas (centelha da vela) na variacao de tempo.
+	RPM.setarElementos(2*(contadorRPM/((millis() - lastMillisRPM)/60000.0)));
 	contadorRPM = 0; //Zerando o contador da interrupcao.
-
-	ligarInterrupcoes(); //Reativando as interrupcoes.
+	lastMillisRPM = millis();
 }
 
 //Funcao de interrupcao da velocidade - INICIO:
-void interrupcao_velocidade()
-{
+void interrupcao_velocidade(){
 	contadorVelocidade++;
 }
 //Funcao de interrupcao da velocidade - FIM.
 
 //Funcao da interrupcao do RPM - INICIO:
-void interrupcao_rpm_motor()
-{ 
+void interrupcao_rpm_motor(){ 
 	contadorRPM++;
 }
-//Funcao da interrupcao do RPM - FIM.
